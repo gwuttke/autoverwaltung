d991f9a32fe44e44395f30ad09f2c6fd4b86a7c3
Georg Wuttke <georg1wuttke@gmail.com>
2015-08-09 21:46:50 +0200

    SQL Script mit Produktivdaten vom 08_08_2015

--- av-server/src/main/resources/db/migration/V001_01__necessary_data.sql
+++ av-server/src/main/resources/db/migration/V001_01__necessary_data.sql
@@ -27,15 +27,6 @@ INSERT INTO `kraftstoffsorte` VALUES (1,'Super'),(2,'Super E10'),(3,'Diesel'),(4
 /*!40000 ALTER TABLE `kraftstoffsorte` ENABLE KEYS */;
 UNLOCK TABLES;
 
---
--- Dumping data for table `ort`
---
-
-LOCK TABLES `ort` WRITE;
-/*!40000 ALTER TABLE `ort` DISABLE KEYS */;
-INSERT INTO `ort` VALUES (1,'M?nchen'),(2,'Neuried'),(3,'keine Angabe'),(4,'Aschheim'),(5,'Dietesheim'),(6,'Annaberg'),(7,'Walchsee'),(8,'Oberschlei?heim'),(9,'Innsbruck'),(10,'San Gregorio Magno'),(11,'Neapel'),(12,'Altenmarkt'),(13,'Bayreuth'),(14,'Pama'),(15,'Casino'),(16,'Modena'),(17,'W?rzburg'),(18,'N?rnberg'),(19,'Hausham'),(20,'Frankfurt am Main'),(21,'Koblenz'),(22,'Andernach'),(23,'Friedrichshafen'),(24,'Heiligenberg'),(25,'Taufkirchen');
-/*!40000 ALTER TABLE `ort` ENABLE KEYS */;
-UNLOCK TABLES;
 
 --
 -- Dumping data for table `kraftstoff`
@@ -55,26 +46,6 @@ LOCK TABLES `tank` WRITE;
 /*!40000 ALTER TABLE `tank` DISABLE KEYS */;
 INSERT INTO `tank` VALUES (1,'1/4'),(2,'1/2'),(3,'3/4'),(4,'4/4');
 /*!40000 ALTER TABLE `tank` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Dumping data for table `land`
---
-
-LOCK TABLES `land` WRITE;
-/*!40000 ALTER TABLE `land` DISABLE KEYS */;
-INSERT INTO `land` VALUES (1,'keine Angabe'),(2,'Deutschland'),(3,'?sterreich'),(4,'Italien'),(5,'Polen');
-/*!40000 ALTER TABLE `land` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Dumping data for table `laenderorte`
---
-
-LOCK TABLES `laenderorte` WRITE;
-/*!40000 ALTER TABLE `laenderorte` DISABLE KEYS */;
-INSERT INTO `laenderorte` VALUES (2,1),(2,2),(2,4),(2,5),(3,6),(3,7),(2,8),(3,9),(4,10),(4,11),(2,12),(2,13),(4,14),(3,15),(4,16),(2,17),(2,18),(2,19),(2,20),(2,21),(2,22),(2,23),(2,24),(2,25);
-/*!40000 ALTER TABLE `laenderorte` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -142,7 +113,10 @@ UNLOCK TABLES;
 
 LOCK TABLES `benutzer` WRITE;
 /*!40000 ALTER TABLE `benutzer` DISABLE KEYS */;
-INSERT INTO `benutzer` VALUES (3,'georg.wuttke','georg1wuttke@gmail.com','Wuttke','12345','Georg',NULL);
+
+INSERT INTO `benutzer` VALUES 
+(1, 'Gechlire', 'lrwuttke@aol.com', 'Wuttke', 'Georg1!', 'Reinhard',1),
+(2, 'georg.wuttke', 'georg1wuttke@gmail.com', 'Wuttke', '12345', 'Georg', 2);
 /*!40000 ALTER TABLE `benutzer` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -152,9 +126,90 @@ UNLOCK TABLES;
 
 LOCK TABLES `benutzer_role` WRITE;
 /*!40000 ALTER TABLE `benutzer_role` DISABLE KEYS */;
-INSERT INTO `benutzer_role` VALUES (3,1);
+INSERT INTO `benutzer_role` VALUES 
+(1,2),
+(2,1);
 /*!40000 ALTER TABLE `benutzer_role` ENABLE KEYS */;
 UNLOCK TABLES;
+
+--
+-- Daten für Tabelle `Land`
+--
+
+INSERT INTO `Land` (`id`, `name`) VALUES
+(1, 'keine Angabe'),
+(2, 'Deutschland'),
+(3, 'Österreich'),
+(4, 'Italien'),
+(5, 'Polen'),
+(6, 'Schweden');
+
+--
+-- Daten für Tabelle `Ort`
+--
+
+INSERT INTO `Ort` (`id`, `ort`) VALUES
+(1, 'München'),
+(2, 'Neuried'),
+(3, 'keine Angabe'),
+(4, 'Aschheim'),
+(5, 'Dietesheim'),
+(6, 'Annaberg'),
+(7, 'Walchsee'),
+(8, 'Oberschleißheim'),
+(9, 'Innsbruck'),
+(10, 'San Gregorio Magno'),
+(11, 'Neapel'),
+(12, 'Altenmarkt'),
+(13, 'Bayreuth'),
+(14, 'Pama'),
+(15, 'Casino'),
+(16, 'Modena'),
+(17, 'Würzburg'),
+(18, 'Nürnberg'),
+(19, 'Hausham'),
+(20, 'Frankfurt am Main'),
+(21, 'Koblenz'),
+(22, 'Andernach'),
+(23, 'Friedrichshafen'),
+(24, 'Heiligenberg'),
+(25, 'Taufkirchen'),
+(26, 'Kaufbeuern');
+
+--
+-- Daten für Tabelle `Laenderorte`
+--
+
+INSERT INTO `Laenderorte` (`idland`, `idort`) VALUES
+(2, 1),
+(2, 2),
+(2, 4),
+(2, 5),
+(3, 6),
+(3, 7),
+(2, 8),
+(3, 9),
+(4, 10),
+(4, 11),
+(2, 12),
+(2, 13),
+(4, 14),
+(3, 15),
+(4, 16),
+(2, 17),
+(2, 18),
+(2, 19),
+(2, 20),
+(2, 21),
+(2, 22),
+(2, 23),
+(2, 24),
+(2, 25);
+
+
+
+
+
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
