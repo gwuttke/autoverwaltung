8ef78869af11ec483d3d43d4a751859120afdfd2
Georg Wuttke <georg1wuttke@gmail.com>
2015-12-30 22:05:12 +0100

    verbesserte Darstellung und Autocomplete beim Hinzufuegen eins Ortes bei einer Tankfuellung

--- av-web/src/main/resources/js/tanken/new.js
+++ av-web/src/main/resources/js/tanken/new.js
@@ -5,6 +5,10 @@ $(function() {
 				format : "dd.mm.yyyy",
 				language:'de'
 			});
+			$('#selectOrt').selectize({
+			    create: false,
+			    sortField: 'text'
+			});
 			updateOrte();
 			
 			$('#selectLand').change(function(){updateOrte();});
@@ -56,13 +60,29 @@ $(function() {
 			type : 'GET',
 			dataType : 'json',
 			success : function(json) {
-				$("#selectOrt").empty();
+				var list = new Array();
+				var ort = {text: 'keine Angabe',
+						value: 0};
+				list.push(ort);
+				$.each(json, function(i, value) {
+					var ort = {text: value.ort,
+							value: value.id};
+					list.push(ort);
+				});				
+				var selectize = $("#selectOrt")[0].selectize;
+				selectize.clear();
+				selectize.clearOptions();
+				selectize.load(function(callback) {
+				    callback(list);
+				});
+				/*
 				$('#selectOrt').append($('<option>').text('keine Angabe').attr('value', '0'));
 				$('#selectOrt').append('</option>');
 				$.each(json, function(i, value) {
 					$('#selectOrt').append($('<option>').text(value.ort).attr('value', value.id));
 					$('#selectOrt').append('</option>');
 				});
+				*/
 			}
 		});
 	};
