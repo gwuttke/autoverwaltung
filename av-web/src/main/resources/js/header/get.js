074d9d364cf3830e19d2fcb8c1bf2fb8b298721a
Georg Wuttke <georg1wuttke@gmail.com>
2017-01-02 19:31:04 +0100

    get js into header js package and add to global html

--- av-web/src/main/resources/js/header/get.js
+++ av-web/src/main/resources/js/header/get.js
@@ -12,24 +12,7 @@ $(function() {
 		var id = $(this).data('id');
 		var token = $("meta[name='_csrf']").attr("content");
 		$('#autoId').val(id);
-		//$.post(urlUpdateCurrent, {});
 		$.post(urlUpdateCurrent, {s: $('#currentAutoForm').serialize(),
 			_csrf: token});
-		//$("#currentAutoForm").ajaxForm({url: urlUpdateCurrent, type: 'post'});
-		/*
-		$.ajax({
-	            type : "POST",
-	            url : urlUpdateCurrent,
-	            data : {autoModelShow: id},
-	            success : function(response) {
-	               
-	            },
-	            error : function(e) {
-	               alert('Failed!: ' + e);
-	            }
-	        }); 
-		*/
 	});
 });
-
-
