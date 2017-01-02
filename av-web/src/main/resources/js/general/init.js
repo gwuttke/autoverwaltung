14a1518701af62ffde17d0dd2de455d4efadbbb7
Georg Wuttke <georg1wuttke@gmail.com>
2017-01-02 16:01:43 +0100

    JS INIT documented and add Config for Selectize

--- av-web/src/main/resources/js/general/init.js
+++ av-web/src/main/resources/js/general/init.js
@@ -1,6 +1,20 @@
+/**
+ * Klasse für initialisierungen von Komponenten über JS
+ */
 var INIT = INIT || {};
 
+/**
+ * Sub-Class field (Initialiesierung von Feldern (z.b. input, datePicker, select)
+ * @memberOf   INIT
+ * @see     INIT
+ * @since   2.0.0
+ */
 INIT.field = {
+	/**
+	 * DatePicker initialisierung 
+	 * @param: {String} inputId - ID des Input Feldes
+	 * @memberOf: {INIT.field}
+	 */
 	datePicker : function(inputId) {
 		var $input = $('#' + inputId);
 
@@ -10,6 +24,12 @@ INIT.field = {
 			todayBtn : 'linked'
 		});
 	},
+	/**
+	 * Selectize initialisierung 
+	 * @param: {String} inputId - ID des Input Feldes
+	 * @param: {function} createFunction(input, callback) - function for new Option creation, if {undefined} creation is {false}
+	 * @memberOf: {INIT.field}
+	 */
 	selectize : function(inputId, createFunction) {
 		var $input = $('#' + inputId);
 		$input.selectize({
@@ -17,6 +37,13 @@ INIT.field = {
 			create : !createFunction ? false :createFunction
 		});
 	},
+	/**
+	 * Numeric initialisierung 
+	 * @param: {String} inputId - ID des Input Feldes
+	 * @param: {Object} data - allowed variables are {value}, {option{mDec as {String}}}
+	 * @Bsp. aufruf: INIT.field.numberic('inputPreisProLiter', {option:{mDec:'3'},value:0});  
+	 * @memberOf: {INIT.field}
+	 */
 	numberic: function(inputId, data){
 		var $input = $('#' + inputId);
 		if(data.option){
@@ -33,6 +60,36 @@ INIT.field = {
 	}
 	
 };
+
+/**
+ * Configuration after Init.field
+ */
+INIT.config = {}
+/**
+ * Config for Selectize
+ */
+INIT.config.selectize = {
+		/**
+		 * ajax JSON create Request 
+		 */
+		ajaxJSONCreateRequest: function(url,input, newData,callback){
+			$.ajax({
+				type : "POST",
+				url : url,
+				contentType: "application/json; charset=utf-8",
+				dataType: 'json',
+				data: JSON.stringify(newData),
+				success : function(result) {
+					if (result>0) {
+						callback({ value: result, text: input });
+					}
+				},
+				error : function(e) {
+					alert('Failed!: ' + e);
+				}
+			});
+		}
+}
 	
 $(function() {
 	var token = $("meta[name='_csrf']").attr("content");
--- av-web/src/main/resources/js/tanken/new.js
+++ av-web/src/main/resources/js/tanken/new.js
@@ -1,3 +1,98 @@
+initNewTanken = function(){
+	INIT.field.datePicker('inputTankenDatum');
+	$('#inputTankenDatum').datepicker('setDate', new Date());
+	
+	INIT.field.selectize('selectLand', function (input, callback){
+		var newData = {text: input}
+		INIT.config.selectize.ajaxJSONCreateRequest(urlAddLand, input, newData, callback)
+	});
+	
+	INIT.field.selectize('selectOrt', function (input, callback){
+		var landID = $('#selectLand').val();
+		var newData = {parent: landID, text: input}
+		INIT.config.selectize.ajaxJSONCreateRequest(urlAddOrt, input, newData, callback)
+	});
+
+	INIT.field.selectize('selectBenzinart');
+	INIT.field.selectize('selectFuellstaendet')
+
+	updateOrte();
+	
+	$("body").on("change","#selectLand",function(){
+		 updateOrte();
+	});
+	
+	INIT.field.numberic('inputKosten', {value:0});
+	INIT.field.numberic('inputLiter', {value:0});
+	INIT.field.numberic('inputPreisProLiter', {option:{mDec:'3'},value:0});
+	
+	$("body").on("change","#inputKosten",function(){
+		if(($('#inputLiter').val().length > 0) && ($(this).val().length > 0)){
+			var preisProLiter = $(this).val() /$('#inputLiter').val() ;
+			$('#inputPreisProLiter').autoNumeric('set',preisProLiter);
+		}		
+	});
+	
+	$("body").on("change","#inputLiter",function(){
+		if($(this).val() != ''){
+			if($('#inputKosten').val().length > 0){
+				var preisProLiter = $(this).val() * $('#inputKosten').val();
+				if(preisProLiter.toString().length === 5){
+					$('#inputPreisProLiter').autoNumeric('set',preisProLiter);
+					return;
+				}
+			}
+			if( $('#inputPreisProLiter').val().length > 0){
+				var kosten = $(this).val() * $('#inputPreisProLiter').val();
+				$('#inputKosten').autoNumeric('set',kosten);
+				return;
+			}
+		}
+	});
+	
+	$("body").on("change","#inputPreisProLiter",function(){
+		if(($('#inputLiter').val().length > 0) && ($(this).val().length > 0)){
+			var kosten = $('#inputLiter').val() * $(this).val();
+			$('#inputKosten').autoNumeric('set',kosten);
+		}
+	});
+	 
+	 $("body").on("click","#btnSaveTanken",function(){
+		 if($('#selectLand').val() === undefined || $('#selectLand').val() === '' ){
+			 $('#selectLand').val(0);
+		 }
+		 if($('#selectOrt').val() === undefined || $('#selectOrt').val() === '' ){
+			 $('#selectOrt').val(0);
+		 }
+		$('#newTankenForm').submit();
+	});
+};
+
+updateOrte = function() {
+	$.ajax({
+		url : urlLaender + $('#selectLand').val(),
+		type : 'GET',
+		dataType : 'json',
+		success : function(json) {
+			var list = new Array();
+			var ort = {text: 'keine Angabe',
+					value: 0};
+			list.push(ort);
+			$.each(json, function(i, value) {
+				var ort = {text: value.ort,
+						value: value.id};
+				list.push(ort);
+			});
+			var selectize = $("body").find("#selectOrt")[0].selectize;
+			selectize.clear();
+			selectize.clearOptions();
+			selectize.load(function(callback) {
+				callback(list);
+			});
+		}
+	});
+};
+
 $(function() {	
 	$('#TankenAdd').click(function() {
 		openModal('newTankenModal', urlNewTanken, function(){
@@ -8,131 +103,6 @@ $(function() {
 	$("body").on("click","#btnSaveTankenCancel",function(){
 		window.location = urlSaveTankenCancel;
 	});
-	
-	initNewTanken = function(){
-		INIT.field.datePicker('inputTankenDatum');
-		$('#inputTankenDatum').datepicker('setDate', new Date());
-		
-		INIT.field.selectize('selectLand', function (input, callback){
-			var newData = {text: input}
-			$.ajax({
-				type : "POST",
-				url : urlAddLand,
-				contentType: "application/json; charset=utf-8",
-				dataType: 'json',
-				data: JSON.stringify(newData),
-				success : function(result) {
-					if (result>0) {
-						callback({ value: result, text: input });
-					}
-				},
-				error : function(e) {
-					alert('Failed!: ' + e);
-				}
-			});
-		});
-
-		INIT.field.selectize('selectOrt', function (input, callback){
-			var landID = $('#selectLand').val();
-			var newData = {parent: landID, text: input}
-			$.ajax({
-				type : "POST",
-				url : urlAddOrt,
-				contentType: "application/json; charset=utf-8",
-				dataType: 'json',
-				data: JSON.stringify(newData),
-				success : function(result) {
-					if (result>0) {
-						callback({ value: result, text: input });
-					}
-				},
-				error : function(e) {
-					alert('Failed!: ' + e);
-				}
-			});
-		});
-
-		INIT.field.selectize('selectBenzinart');
-		
-		INIT.field.selectize('selectFuellstaendet')
-
-		updateOrte();
-		
-		$("body").on("change","#selectLand",function(){
-			 updateOrte();
-		});
-		
-		INIT.field.numberic('inputKosten', {value:0});
-		INIT.field.numberic('inputLiter', {value:0});
-		INIT.field.numberic('inputPreisProLiter', {option:{mDec:'3'},value:0});
-		
-		$("body").on("change","#inputKosten",function(){
-			if(($('#inputLiter').val().length > 0) && ($(this).val().length > 0)){
-				var preisProLiter = $(this).val() /$('#inputLiter').val() ;
-				$('#inputPreisProLiter').autoNumeric('set',preisProLiter);
-			}		
-		});
-		
-		$("body").on("change","#inputLiter",function(){
-			if($(this).val() != ''){
-				if($('#inputKosten').val().length > 0){
-					var preisProLiter = $(this).val() * $('#inputKosten').val();
-					if(preisProLiter.toString().length === 5){
-						$('#inputPreisProLiter').autoNumeric('set',preisProLiter);
-						return;
-					}
-				}
-				if( $('#inputPreisProLiter').val().length > 0){
-					var kosten = $(this).val() * $('#inputPreisProLiter').val();
-					$('#inputKosten').autoNumeric('set',kosten);
-					return;
-				}
-			} 
-			
-		});
-		
-		 $("body").on("change","#inputPreisProLiter",function(){
-			if(($('#inputLiter').val().length > 0) && ($(this).val().length > 0)){
-				var kosten = $('#inputLiter').val() * $(this).val();
-				$('#inputKosten').autoNumeric('set',kosten);
-			}
-		});
-		 
-		 $("body").on("click","#btnSaveTanken",function(){
-			 if($('#selectLand').val() === undefined || $('#selectLand').val() === '' ){
-				 $('#selectLand').val(0);
-			 }
-			 if($('#selectOrt').val() === undefined || $('#selectOrt').val() === '' ){
-				 $('#selectOrt').val(0);
-			 }
-			$('#newTankenForm').submit();
-		});
-	};
-		
-	updateOrte = function() {
-		$.ajax({
-			url : urlLaender + $('#selectLand').val(),
-			type : 'GET',
-			dataType : 'json',
-			success : function(json) {
-				var list = new Array();
-				var ort = {text: 'keine Angabe',
-						value: 0};
-				list.push(ort);
-				$.each(json, function(i, value) {
-					var ort = {text: value.ort,
-							value: value.id};
-					list.push(ort);
-				});				
-				var selectize = $("body").find("#selectOrt")[0].selectize;
-				selectize.clear();
-				selectize.clearOptions();
-				selectize.load(function(callback) {
-					callback(list);
-				});
-			}
-		});
-	};
 	
 	initNewTanken();
 });
